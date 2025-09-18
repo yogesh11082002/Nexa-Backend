161f7c5b242abf0ccc5c428004a1f053c09b42f7
Yogesh <yogeshthakur938920@gmail.com>
2025-09-18 09:35:40 +0530

    good

--- configs/multer.js
+++ configs/multer.js
@@ -1,5 +1,23 @@
-import multer from "multer";
+// import multer from "multer";
 
-const storage = multer.diskStorage();
+// const storage = multer.diskStorage();
+// const upload = multer({ storage });
+// export default upload;
+import multer from "multer";
+import path from "path";
+
+// Configure storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // folder where files will be saved
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+// Multer middleware
 const upload = multer({ storage });
+
 export default upload;
